593a67c59850c0ce2b7ce4482858f445bcbcbcd3
Tobias Roman <98361447+TobiasRoman@users.noreply.github.com>
2022-04-02 21:55:08 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const process = require('process');
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+/* Enrutadores */
+const indexRouter = require('./routes/indexRouter');
+const productsRouter = require('./routes/productsRouter')
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+/* Views config */
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, "views"))
+
+
+/* Middlewares de Rutas */
+app.use('/', indexRouter) // HOME - Contact 
+app.use('/productos', productsRouter) // Listado, detalle
+
+app.listen(PORT, () => console.log(`
+Server listen port ${PORT}
+http://localhost:${PORT}
+`))
